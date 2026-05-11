70071a13da5ab73aea9d07824016b50e66c2b955
SunWe <82327121+SunWe-success@users.noreply.github.com>
2026-05-11 08:32:37 +0800

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ModelsToJson.Views;
+using Prism.Ioc;
+using Prism.Navigation.Regions;
+using System.Text;
+using System.Windows;
+
+namespace ModelsToJson
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<ModelToJsonView>();
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            IRegionManager regionManager = Container.Resolve<IRegionManager>();
+            regionManager.RegisterViewWithRegion("AnyModelToJsonView", typeof(ModelToJsonView));
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        }
+    }
+}
